762ae5027b747412d5e8b16508ac67eac9c0fcc0
janetcushing <janet.cushing@gmail.com>
2018-01-03 18:04:53 -0500

    added sql files and they exeute successfully

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,7 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
+
 CREATE TABLE burgers_t
 (
 	id INT NOT NULL AUTO_INCREMENT,
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,5 +5,4 @@ INSERT INTO `burgers_db`.`burgers_t` (`burger_name`) VALUES
 ,  ("Bulger Wheat Burger with Guacamole and Peppers")
 ,  ("Black Bean Chipotle Burger");
 
-
 SELECT * FROM `burgers_db`.`burgers_t`;
