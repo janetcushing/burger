f2fefb37c4e098afd353e8014b50d87fb9cbd715
janetcushing <janet.cushing@gmail.com>
2018-01-09 16:05:57 -0500

    added JAWSDB_URL

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "",
     database: "burgers_db"
 });
+};
 
 connection.connect(function (err) {
     if (err) {
@@ -15,12 +21,6 @@ connection.connect(function (err) {
       }
     console.log("I have successfully connected to the burgers_db");
     console.log("connected as id " + connection.threadId);
-    // console.log("-----------------------------------------");
-    // console.log(" ");
-    // console.log("  Thank you for eating at Eat-Da-Burger!!");
-    // console.log(" ");
-    // console.log("-----------------------------------------");
-    // connection.end();
 });
 
 module.exports = connection;
