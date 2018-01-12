4d1d6ae9cc31470a8939581a8790ef974c78b2d7
janetcushing <janet.cushing@gmail.com>
2018-01-12 14:22:59 -0500

    code is complete

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -15,86 +15,84 @@ var router = express.Router();
 // Routes
 // =============================================================
 
-      router.get("/", function(req, res) {
-          console.log("im in the get/");
-        burger.all(function(data) {
-          var uneaten = [];
-          var eaten = [];
-          console.log(data.length);
-          for (let i = 0; i < data.length; i++) {
-            if (data[i].devoured) {
-                eaten.push(data[i])
-            } else {
-                uneaten.push(data[i])
+router.get("/", function (req, res) {
+    burger.all(function (data, err) {
+        if (err) {
+            // If an error occurred, send a generic server failure
+            console.log(err);
+            res.status(500).end();
+        } else {
+            var uneaten = [];
+            var eaten = [];
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].devoured) {
+                    eaten.push(data[i])
+                } else {
+                    uneaten.push(data[i])
+                }
             }
+            res.render("index", {
+                uneatenBurger: uneaten,
+                eatenBurger: eaten
+            });
         }
-        console.log("uneaten " + JSON.stringify(uneaten));
-        res.render("index", {
-            uneatenBurger: uneaten,
-            eatenBurger: eaten
-        });
-        });
-      });
-
-      router.get("/index", function(req, res) {
-        burger.all(function(data) {
-        //   var hbsObject = {
-        //     burgers: data
-        //   };
-        //   console.log(hbsObject);
-          var uneaten = [];
-          var eaten = [];
-          for (let i = 0; i < data.length; i++) {
-            if (data[i].devoured) {
-                eaten.push(data[i])
-            } else {
-                uneaten.push(data[i])
-            }
-        }
-        res.render("index", {
-            uneatenBurger: uneaten,
-            eatenBurger: eaten
-        });
-        });
-      });
-
-
-    // add a new burger
-    router.post("/api/burgers", function (req, res) {
-        console.log("im adding a new burgerzzz");
-       
-       
-        // newBurger = req.body;
-        // console.log("newBurger: " + newBurger);
-        // var burgerObject = {
-        //     "burger_name": req.body.burger_name
-        //   };
-        //   console.log(newBurger);
-        burger.create([req.body.burger_name], function(result){
-            res.status(200).end();
-        }); 
     });
+});
 
-    // update the devoured status
-    router.put("/api/devoured/:id", function (req, res) {
-        console.log("im devouring a burger");
-        // update the DB.
-        var burgerId = req.params.id;
-        console.log("id " + burgerId);
-        burger.update(burgerId, function(data, err) {
-            if (err) {
-                // If an error occurred, send a generic server faliure
-                console.log(err);
-                return res.status(500).end();
-            } else if (data.changedRows == 0) {
-                console.log(data);
-                // If no rows were changed, then the ID must not exist, so 404
-                console.log("burger row did not get updated");
-                return res.status(404).end();
-            } else {
-                console.log("updated the burger");
-                res.status(200).end();
+router.get("/index", function (req, res) {
+    burger.all(function (data, err) {
+        if (err) {
+            // If an error occurred, send a generic server failure
+            console.log(err);
+            res.status(500).end();
+        } else {
+            var uneaten = [];
+            var eaten = [];
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].devoured) {
+                    eaten.push(data[i])
+                } else {
+                    uneaten.push(data[i])
+                }
             }
+            res.render("index", {
+                uneatenBurger: uneaten,
+                eatenBurger: eaten
+            });
+        }
+    });
+});
+
+
+// add a new burger
+router.post("/api/burgers", function (req, res) {
+    burger.create([req.body.burger_name], function (data, err) {
+        if (err) {
+            // If an error occurred, send a generic server failure
+            console.log(err);
+            res.status(500).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+// update the devoured status
+router.put("/api/devoured/:id", function (req, res) {
+    // var burgerId = req.params.id;
+    burger.update(req.params.id, function (data, err) {
+        if (err) {
+            // If an error occurred, send a generic server failure
+            console.log(err);
+            return res.status(500).end();
+        } else if (data.changedRows == 0) {
+            console.log(data);
+            // If no rows were changed, then the ID must not exist, so 404
+            console.log("burger row did not get updated");
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
     });
 });
 
--- db/schema.sql
+++ db/schema.sql
@@ -2,14 +2,14 @@
 --- Database Schema
 --------------------------------------------
 
-DROP   DATABASE burgers_db;
+DROP   DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
 CREATE TABLE burgers_t
 (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INTEGER  AUTO_INCREMENT NOT NULL,
 	burger_name VARCHAR(255) NOT NULL,
 	devoured BOOLEAN NOT NULL default false,
 	burger_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
